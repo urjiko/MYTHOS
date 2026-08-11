14ff0bfa406f08e22f1c648b61e32bce2f74aa65
urjiko <31261795+urjiko@users.noreply.github.com>
2026-08-11 13:58:37 +0300

    Load the Daidalos and Ikaros scene at startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './registerDaidalosIkaros'
 import App from './App'
 import { registerMythosServiceWorker } from './pwa'
 import { applyTurkishCopyPolish } from './turkishCopyPolish'
