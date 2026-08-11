2cad3e5c753f3bfdf5bb1e502175e43f1ab1f470
urjiko <31261795+urjiko@users.noreply.github.com>
2026-08-11 14:03:04 +0300

    Keep Daidalos catalog summary lightweight on startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './registerDaidalosIkaros'
+import './registerDaidalosIkarosSummary'
 import App from './App'
 import { registerMythosServiceWorker } from './pwa'
 import { applyTurkishCopyPolish } from './turkishCopyPolish'
